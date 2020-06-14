4a3a16af968beff1665e34295b3ea8337dd9b9fd
marcos <marcoskim60@gmail.com>
2020-06-14 00:04:44 -0300

    Performing actions, Actions panel

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -3,41 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using NavGame.Core;
+using NavGame.Managers;
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : TouchableGameObject
 {
     NavMeshAgent agent;
     Camera cam;
-
     public float range = 4f;
-
     public LayerMask walkableLayer;
     public LayerMask collectibleLayer;
-
     CollectibleGameObject pickupTarget;
-
     Vector3 actionPoint = Vector3.zero;
-
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
     }
-
-
     void Update()
     {
         ProcessInput();
         UpdateCollect();
         UpdateAction();
     }
-
     void ProcessInput()
     {
         if (Input.GetMouseButtonDown(1))
         {
+            LevelManager.instance.CancelAction();
+            actionPoint = Vector3.zero;
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
             {
                 agent.SetDestination(hit.point);
@@ -52,11 +50,11 @@ public class PlayerController : TouchableGameObject
                 pickupTarget = null;
             }
         }
-        else if (Input.GetMouseButtonDown(0))
+       
+        else if (Input.GetMouseButtonDown(0) && LevelManager.instance.IsActionSelected())
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
             {
                 actionPoint = hit.point;
@@ -64,6 +62,18 @@ public class PlayerController : TouchableGameObject
             }
         }
 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            LevelManager.instance.SelectedAction(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            LevelManager.instance.SelectedAction(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LevelManager.instance.SelectedAction(2);
+        }
 
     }
 
@@ -77,7 +87,6 @@ public class PlayerController : TouchableGameObject
             }
         }
     }
-
     void UpdateAction()
     {
         if (actionPoint != Vector3.zero)
@@ -85,9 +94,9 @@ public class PlayerController : TouchableGameObject
             if (Vector3.Distance(transform.position, actionPoint) <= range)
             {
                 agent.ResetPath();
+                LevelManager.instance.DoAction(actionPoint);
                 actionPoint = Vector3.zero;
             }
         }
     }
-
 }
